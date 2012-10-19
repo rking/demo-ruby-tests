6311f6a9041966b109cce0875d6903e129afbff4
☈king <rking@sharpsaw.org>
2012-10-19 03:01:33 -0500

    Switch to plymouth (?)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,3 @@
-require 'minitest/unit'
-require 'pry-rescue'
-Pry.rescue do
-  # This line comes from (what I think is) the important part of
-  # https://github.com/seattlerb/minitest/blob/master/lib/minitest/autorun.rb
-  MiniTest::Unit.autorun
-end
+require 'minitest/autorun'
+require 'plymouth'
 $:.unshift './lib'
