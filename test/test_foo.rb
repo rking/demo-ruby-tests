cf84639e8a502916bffe41407962db9bb63c3b0d
☈king <rking@sharpsaw.org>
2012-10-23 22:37:43 -0500

    Remove needless ::

--- test/test_foo.rb
+++ test/test_foo.rb
@@ -2,6 +2,6 @@ require 'test_helper'
 require 'foo'
 class TestFoo < MiniTest::Unit::TestCase
   def test_foo_one
-    assert_equal ::Foo.new.hi, 'Oh, Hai!'
+    assert_equal Foo.new.hi, 'Oh, Hai!'
   end
 end
